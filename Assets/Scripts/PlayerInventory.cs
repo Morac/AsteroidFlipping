e2f39a5085698c55c65c03709228b06a360f957a
D. Max Loy <morac@morac.net>
2014-09-06 00:43:52 -0700

    Added ngui. Started buy process.

---
+++ Assets/Scripts/PlayerInventory.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class PlayerInventory 
+{
+
+	const string FundsKey = "Player.Funds";
+	static int _funds = -1;
+	public static int Funds
+	{
+		get
+		{
+			if(_funds == -1)
+			{
+				_funds = PlayerPrefs.GetInt(FundsKey, 0);
+			}
+			return _funds;
+		}
+		set
+		{
+			_funds = value;
+			PlayerPrefs.SetInt(FundsKey, value);
+		}
+	}
+
+	public static bool CanAfford(int val)
+	{
+		return Funds - val > 0;
+	}
+}
