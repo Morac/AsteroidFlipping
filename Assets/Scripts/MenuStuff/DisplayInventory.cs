5057db2449fca6418802348fc03f4772d9d34d13
D. Max Loy <morac@morac.net>
2014-09-07 13:06:55 -0700

    Added Items to inventory, moved some things.

---
+++ Assets/Scripts/MenuStuff/DisplayInventory.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(UILabel))]
+public class DisplayInventory : MonoBehaviour {
+
+	UILabel label;
+
+	void Start()
+	{
+		label = GetComponent<UILabel>();
+
+		PlayerInventory.ItemChangedCallback += InvetoryUpdated;
+		label.text = PlayerInventory.inventory.ToString();
+	}
+
+	void OnDestroy()
+	{
+		PlayerInventory.ItemChangedCallback -= InvetoryUpdated;
+	}
+
+	void InvetoryUpdated(PlayerInventory.Item m, int val)
+	{
+		label.text = PlayerInventory.inventory.ToString();
+	}
+}
--- Assets/Scripts/PlayerInventory.cs
+++ Assets/Scripts/PlayerInventory.cs
@@ -1,10 +1,21 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public static class PlayerInventory 
 {
+	public enum Item
+	{
+		Metals,
+		Organics,
+		Seeds,
+		Electronics
+	}
+
 	public delegate void FundsChanged(int newval);
 	public static FundsChanged FundsChangedCallback;
+
+	public delegate void ItemChanged(Item m, int newval);
+	public static ItemChanged ItemChangedCallback;
 
 	const string FundsKey = "Player.Funds";
 	static int _funds = -1;
@@ -31,6 +42,56 @@ public static class PlayerInventory
 
 	public static bool CanAfford(int val)
 	{
-		return Funds - val >= 0;
+		return Funds >= val;
+	}
+
+	public static bool CanAfford(Item m, int val)
+	{
+		return inventory[m] >= val;
+	}
+
+	public static string ItemKey(Item m)
+	{
+		return "Player." + m.ToString();
+	}
+
+	public static Inventory inventory = new Inventory();
+
+	public class Inventory
+	{
+		Dictionary<Item, int> counts = new Dictionary<Item, int>();
+
+		public Inventory()
+		{
+			foreach (Item m in System.Enum.GetValues(typeof(Item)))
+			{
+				counts[m] = PlayerPrefs.GetInt(ItemKey(m), 0);
+			}
+		}
+
+		public int this[Item m]
+		{
+			get
+			{
+				return counts[m];
+			}
+			set
+			{
+				counts[m] = value;
+				PlayerPrefs.SetInt(ItemKey(m), value);
+				if (ItemChangedCallback != null)
+					ItemChangedCallback(m, value);
+			}
+		}
+
+		public override string ToString()
+		{
+			string s = " ";
+			foreach(var m in counts)
+			{
+				s += m.Key + ": " + m.Value + " ";
+			}
+			return s;
+		}
 	}
 }
